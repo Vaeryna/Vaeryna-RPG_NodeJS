4d1caead895cb82dedb042120b7e5e934dc7af33
Vaeryna <leblond.melanie@gmail.com>
2022-09-02 16:54:39 +0200

    16h54

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import http from "http"
 import pug from "pug"
+import setCharacter from "./utils/setCharacters.js";
 
+import body2Obj from "./utils/body2Obj.js";
+
+let name = ""
+let difficulty = ""
+let classe = ""
+let hydra = setCharacter("hydra");
+let player;
+let html;
+let turn = 0
 
 const server = http.createServer((req, res) => {
         let url = req.url.replace('/', '');
@@ -9,46 +19,57 @@ const server = http.createServer((req, res) => {
             let html = pug.renderFile('./views/home.pug', {bool: true});
 
             res.end(html)
-
         }
         if (url === "newGame") {
             let html = pug.renderFile('./views/newGame.pug');
             res.end(html)
         }
+
         if (url === 'fight') {
-            pug.renderFile('./views/fight.pug', {
-                name: "Thrall",
-                work: "Chaman",
-                difficulty: "easy",
-                PlayerStats: {
-                    health: 100,
-                    armor: 15,
-                    magic: 50,
-                    strength: 15,
-                    speed: 20
-                },
-                HydraStats: {
-                    head: 3,
-                    health: 100,
-                    armor: 15,
-                    magic: 50,
-                    strength: 15,
-                    speed: 20
-                }
-
-            }, (err, data) => {
-                if (err) return console.log(err.message);
-
-                res.end(data);
+            let body = [];
+            if (req.method === 'POST') {
+                req.on('data', chunk => {
+                    body.push(chunk);
 
 
-            })
-        } else {
-            res.end()
+                }).on('end', () => {
+                    body = Buffer.concat(body);
+                    body = body2Obj(body);
+                    console.log(body)
+                    name = body.name
+                    difficulty = body.difficulty
+                    classe = body.class
+
+                    let player = setCharacter(name)
+
+                     html = pug.renderFile('./views/fight.pug', {
+                        name: name,
+                        work: classe,
+                        difficulty: difficulty,
+                        PlayerStats: {
+                            health: player.health,
+                            armor: hydra.armor,
+                            magic: hydra.magic,
+                            strength: hydra.strength,
+                            speed: hydra.speed
+                        },
+                        HydraStats: {
+                            head: hydra.head,
+                            health: hydra.health,
+                            armor: hydra.armor,
+                            magic: hydra.magic,
+                            strength: hydra.strength,
+                            speed: hydra.speed
+                        }, turn
+                    })
+
+                })
+                res.end(html)
+            } else {
+                res.end()
+            }
         }
-
     }
 )
-
 console.log("Server running at http://127.0.0.1:8000");
 server.listen(8000)

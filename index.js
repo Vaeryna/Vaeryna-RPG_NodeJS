3d41b3b09b71acad98f10aa4256a6a8421dc58b4
Vaeryna <leblond.melanie@gmail.com>
2022-09-02 11:32:31 +0200

    Mel - ajout de fichiers views + chemin arraché

--- index.js
+++ index.js
@@ -1,73 +1,39 @@
-import figlet from "figlet";
 import http from "http"
-import prompt from "./utils/game.js"
 import pug from "pug"
 
 
 const server = http.createServer((req, res) => {
-    let url = req.url.replace('/', '');
-    res.setHeader("Content-Type", "text/html");
-    if (url === "") {
-        let html = pug.renderFile('./views/home.pug', {bool: true});
+        let url = req.url.replace('/', '');
+        res.setHeader("Content-Type", "text/html");
+        if (url === "") {
+            let html = pug.renderFile('./views/home.pug', {bool: true});
 
-        res.end(html)
+            res.end(html)
 
+        }
+        if (url === "newGame") {
+            let html = pug.renderFile('./views/newGame.pug');
+            res.end(html)
+        }
+        if (url === 'fight') {
+            let html = pug.renderFile('./views/fight.pug', {
+                name: "Thrall",
+                work: "Chaman",
+                difficulty: "easy"
+            }, (err, data) => {
+                if (err) return console.log(err.message);
+
+                res.end(data);
+
+
+
+            })
+        } else {
+            res.end()
+        }
 
     }
-    if (url === "newGame") {
-        let html = pug.renderFile('./views/newGame.pug');
-        res.end(html)
-    }
-})
-
-const options = {
-    hostname: '127.0.0.1',
-    port: 8000,
-    path: '/begin',
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-};
-const data = JSON.stringify({
-    name: 'John Doe',
-    job: 'DevOps Specialist'
-});
-
-const req = http.request(options, (res) => {
-    let data = '';
-
-    res.on('data', (chunk) => {
-        data += chunk;
-    });
-    console.log(data)
-
-    res.on('end', () => {
-        console.log(JSON.parse(data));
-    });
-
-}).on("error", (err) => {
-    console.log("Error: ", err.message);
-});
-
-
-
-req.end();
-
-// start process
-// figlet("Jeu", (err, data) => {
-//   if (err) return console.log(err);
-//
-//   // clear terminal
-//   const lines = process.stdout.getWindowSize()[1];
-//   for (let i = 0; i < lines; i++) console.log("\r\n");
-//
-//   console.log(data);
-//   console.log("\n");
-//
-//   // call recursion
-//   prompt("list", "What difficulty do you want", ["Easy", "Medium", "Hard"], 1);
-// });
+)
 
 console.log("Server running at http://127.0.0.1:8000");
 server.listen(8000)
